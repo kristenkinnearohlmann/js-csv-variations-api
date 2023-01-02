34f49a3668b6cdb1b029c28087463d9204097d5a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-01-01 18:24:45 -0600

    Complete basic server setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS CSV Variations API
+
+<https://opensource.com/article/22/7/javascript-api-express>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+const port = 5001; // using 5000 for site
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

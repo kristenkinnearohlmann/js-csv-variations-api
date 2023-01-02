bd034ea2ba1c8313598b5a8e32ce8d1e61dd82c8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-01-01 18:52:36 -0600

    Add body-parser lib, confirm receipt of request body

--- src/file-import.js
+++ src/file-import.js
@@ -1,6 +1,6 @@
 const fileImport = async (req) => {
   console.log("Reached file input");
-  console.log(req.data);
+  console.log(req.body);
   return "Reached me";
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { fileImport } = require("./file-import");
 const app = express();
 const port = 5001; // using 5000 for site
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,6 +20,7 @@ app.get("/file-import", (req, res) => {
 });
 
 app.post("/file-import", async (req, res) => {
+  console.log("Is there something here?", req.body);
   var result = await fileImport(req);
   res.json({ msg: "Msg coming soon", result: result });
 });

9332a389ba4488fd1af15314093b466ccc2bfd55
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-01-03 22:12:47 -0600

    Clean up unused work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const multer = require("multer");
-const upload = multer({ dest: "./uploads" });
 const { fileImport } = require("./file-import");
 const app = express();
 const port = 5001; // using 5000 for site
@@ -22,18 +20,3 @@ app.get("/file-import", (req, res) => {
 app.post("/file-import", async (req, res, next) => {
   console.log(req.body);
 });
-
-// app.post("/file-import", upload.single("file"), async (req, res, next) => {
-//   console.log(req.file);
-//   // const form = formidable({ multiples: true });
-//   // form.parse(req, (err, fields, files) => {
-//   //   if (err) {
-//   //     console.error(String(err));
-//   //   }
-//   //   res.end(JSON.stringify({ fields, files }, null, 2));
-//   // });
-
-//   // return;
-//   // var result = await fileImport(req);
-//   // res.json({ msg: "Msg coming soon", result: result });
-// });

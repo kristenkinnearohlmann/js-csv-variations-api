0220dde9c2cef409a90ca5f997b51cb376eeedfe
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-01-09 16:32:26 -0600

    Add todo for processing incoming request

--- src/process-data.js
+++ src/process-data.js
@@ -1,6 +1,7 @@
 const processData = async (req) => {
   console.log("Reached file input");
   console.log(req.body);
+  // TODO: Handle request, return useful response
   return { msg: "End of line" };
 };
 

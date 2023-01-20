8305353425b7cf0f13f59f08991f27a16378c479
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-01-19 18:15:53 -0600

    Add todo

--- src/process-data.js
+++ src/process-data.js
@@ -2,6 +2,7 @@ const processData = async (req) => {
   console.log("Reached file input");
   console.log(req.body);
   // TODO: Handle request, return useful response
+  // TODO: Stringify back and forth
   return { status: 200, body: { msg: "End of line" } };
 };
 
